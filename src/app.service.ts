780337543be7eabc94ddc411e6d82a84d136551a
150110059 <mustafaakgilli@gmail.com>
2022-08-16 00:22:01 +0300

    mustafa

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Ziraat! 111111';
+    return 'Hello Ziraat! 22222';
   }
 }
